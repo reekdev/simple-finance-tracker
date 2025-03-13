9133f835467f63a5dde102ba5abeb94008e4f0aa
Reekdev Ray <reekdev.ray@vyrazu.com>
2025-03-13 18:09:48 +0530

    feat: implement mobile sidebar background transition

--- src/app/auth-layout/ui/footer/footer.component.ts
+++ src/app/auth-layout/ui/footer/footer.component.ts
@@ -1,12 +1,22 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, inject } from '@angular/core';
+import { SidebarToggleService } from '../../data-access/sidebar-toggle/sidebar-toggle.service';
 
 @Component({
   selector: 'app-footer',
   imports: [],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '[attr.sidebar-state]': `sidebarState()`
+  }
 })
 export class FooterComponent {
+  readonly #sidebarToggleService = inject(SidebarToggleService);
+  sidebarState = this.#sidebarToggleService.sidebarState;
 
+  e = effect(() => {
+    console.log('FooterComponent: effect');
+    console.log(this.sidebarState());
+  });
 }
